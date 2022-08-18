eec302ee62a45194548e26fbfa6d18b14eb7e7ab
Dave <davidnjau21@gmail.com>
2022-08-18 18:58:17 +0300

    first commit

---
+++ src/main/java/com/example/circleci/Controller.java
@@ -0,0 +1,15 @@
+package com.example.circleci;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+    @GetMapping("/")
+    public String getHello() {
+        return "Hello World";
+    }
+
+}
