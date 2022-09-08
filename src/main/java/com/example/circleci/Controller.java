c62db93aa7ae5307924bd4425a7e5db2eeeba1d1
Dave <davidnjau21@gmail.com>
2022-09-08 12:02:38 +0300

    Sam

--- src/main/java/com/example/circleci/Controller.java
+++ src/main/java/com/example/circleci/Controller.java
@@ -1,15 +1,16 @@
 package com.example.circleci;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
 
 
-    @GetMapping("/")
-    public String getHello() {
-        return "Hello World";
-    }
+    @RequestMapping(value = "/")
+    public String uploadApk() {
 
+        return "index";
+    }
 }
